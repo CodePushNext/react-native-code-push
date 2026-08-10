43aebb5a70bc860bfa235751b98e7ae4fcf0d83c
Minsik Kim <kmsbernard@gmail.com>
2026-08-10 15:07:16 +0900

    Fix Expo config plugin dependency resolution

--- expo.js
+++ expo.js
@@ -5,7 +5,7 @@ const {
   withAppBuildGradle,
   withMainApplication, // Using the safer, higher-level helper
   WarningAggregator,
-} = require('@expo/config-plugins');
+} = require('expo/config-plugins');
 const fs = require('fs');
 const path = require('path');
 
